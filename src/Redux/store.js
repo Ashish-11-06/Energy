b9970378832aa217ec386d3e9c23533da1c45d29
Ashish Bhosale <bhosaleashish311@gmail.com>
2024-12-18 14:58:06 +0530

    .

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,9 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import loginReducer from './Slices/logInSlice';
+// import loginReducer from './Slices/logInSlice';
 
 export const store = configureStore({
   reducer: {
-    auth: loginReducer, // Add your reducers here
+    // auth: loginReducer, // Add your reducers here
   },
 });
 
