4a0757983ef92a3b0689f48976fdb565fb995f50
Ashish Bhosale <bhosaleashish311@gmail.com>
2024-12-20 16:50:45 +0530

    Add dummy reducer to Redux store for testing purposes

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,9 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
-// import loginReducer from './Slices/logInSlice';
+
+// Dummy reducer
+const dummyReducer = (state = {}, action) => {
+  switch (action.type) {
+    case 'dummy/action':
+      return { ...state, dummyData: action.payload };
+    default:
+      return state;
+  }
+};
 
 export const store = configureStore({
   reducer: {
-    // auth: loginReducer, // Add your reducers here
+    dummy: dummyReducer, // Add the dummy reducer here
   },
 });
 
