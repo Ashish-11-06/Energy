56b75287f094cdf5567e5361c6b3da63f47fe9be
Ashish Bhosale <bhosaleashish311@gmail.com>
2025-02-20 20:34:34 +0530

    landing page

--- src/Components/Modals/Registration/RegisterForm.jsx
+++ src/Components/Modals/Registration/RegisterForm.jsx
@@ -97,8 +97,8 @@ const RegisterForm = ({ open, onCancel, onCreate }) => {
       onCancel={handleCancel}
       width={700}
       style={{
-        marginLeft: '55%',
-        marginTop: '4%',
+        // marginLeft: '55%',
+        // marginTop: '4%',
       }}
     >
       <Form form={form} layout="vertical" name="consumer_registration_form"
