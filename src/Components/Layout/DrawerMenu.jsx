2ff1d0c56961f4eb9f5b9494d5c8308970d5f95f
Ashish Bhosale <bhosaleashish311@gmail.com>
2025-02-21 11:25:50 +0530

    added infographics

--- src/Components/Layout/DrawerMenu.jsx
+++ src/Components/Layout/DrawerMenu.jsx
@@ -1,41 +1,19 @@
 import React from 'react';
-import { Drawer, Layout, Menu } from 'antd';
+import { Drawer, Menu } from 'antd';
 import { useLocation } from 'react-router-dom';
 import { consumerMenuItems, generatorMenuItems } from './MenuItems';
-import { Header } from 'antd/es/layout/layout';
 
 const DrawerMenu = ({ drawerVisible, toggleDrawer }) => {
   const location = useLocation();
 
   // Determine menu type based on URL
-  // const menuType = location.pathname.startsWith('/consumer') ? 'consumer' : 'generator';
-const menuType=JSON.parse(localStorage.getItem('user'))?.user?.user_category;
-// console.log(menuType);
+  const menuType = location.pathname.startsWith('/consumer') ? 'consumer' : 'generator';
 
   // Select menu items based on the type
-  const menuItems = menuType === 'Consumer' ? consumerMenuItems : generatorMenuItems;
+  const menuItems = menuType === 'consumer' ? consumerMenuItems : generatorMenuItems;
 // console.log("Menu items ",menuItems);
 
   return (
-    // <Drawer
-    //   title="Menu"
-    //   placement="left"
-    //   closable
-    //   onClose={toggleDrawer}
-    //   open={drawerVisible}
-    //   contentStyle={{ padding: 0 }} // Updated for Ant Design v5
-    // >
-    //   <Menu theme="light" mode="inline" items={menuItems} />
-    // </Drawer>
-    <Layout>
-    <Header style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: 0 }}>
-      <div style={{ color: 'white', fontSize: '18px' }}>Company Name</div>
-      <div>
-        {/* Drawer trigger button */}
-        <button onClick={toggleDrawer}>☰</button>
-      </div>
-    </Header>
-
     <Drawer
       title="Menu"
       placement="left"
@@ -46,7 +24,6 @@ const menuType=JSON.parse(localStorage.getItem('user'))?.user?.user_category;
     >
       <Menu theme="light" mode="inline" items={menuItems} />
     </Drawer>
-  </Layout>
   );
 };
 
--- src/Pages/Consumer/AnnualSaving.jsx
+++ src/Pages/Consumer/AnnualSaving.jsx
@@ -1,14 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { Button, Spin, message, Typography, Space, Tooltip, Col, Row } from "antd";
+import { Button, Spin, message, Typography, Col, Row, Tooltip } from "antd";
 import { useNavigate } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { FetchAnnualSaving } from "../../Redux/Slices/Consumer/AnnualSavingSlice";
 import { FileTextOutlined } from "@ant-design/icons";
-import chat from '../../assets/chatAnnual.png';
 import { generatePDF, createPdfContent } from './utils'; // Import from utils.js
 import DemandModal from "./Modal/DemandModal";
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip as RechartsTooltip, Legend, PieChart, Pie, Cell } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip as RechartsTooltip, Legend, LineChart, Line } from 'recharts';
 
 const { Title, Text } = Typography;
 
@@ -67,24 +66,81 @@ const AnnualSvg = () => {
   };
 
   const dataForBarChart = [
-    { name: 'Potential Savings', value: annualSavingResponse?.annual_savings || 0 },
-    { name: 'Average Savings', value: annualSavingResponse?.average_savings || 0 },
+    { name: 'Potential Savings', Savings: annualSavingResponse?.annual_savings || 0 },
+    { name: 'Average Savings', Savings: annualSavingResponse?.average_savings || 0 },
   ];
 
-  const dataForPieChart = [
-    { name: 'RE Replacement', value: annualSavingResponse?.re_replacement || 0 },
-    { name: 'Non-RE', value: 100 - (annualSavingResponse?.re_replacement || 0) },
+  const dataForTariffComparison = [
+    { name: 'Your Electricity Tariff', Tarrif: annualSavingResponse?.electricity_tariff || 0 },
+    { name: 'Potential RE Tariff', Tarrif: annualSavingResponse?.potential_re_tariff || 0 },
   ];
 
-  const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+  const dataForSavingsBreakdown = [
+    { name: 'Per Unit Savings', Breakdown: annualSavingResponse?.per_unit_savings_potential || 0 },
+    { name: 'ISTS Charges', Breakdown: annualSavingResponse?.ISTS_charges || 0 },
+    { name: 'State Charges', Breakdown: annualSavingResponse?.state_charges || 0 },
+  ];
 
   return (
     <div>
       <Spin spinning={loading} tip="Loading...">
         <main style={{ padding: "20px", minHeight: "90vh", position: "relative" }}>
-          <Row justify="center" align="middle">
+          {/* Infographics Section */}
+          <Row gutter={[16, 16]} justify="center" style={{ marginTop: "20px", background: 'white', border: "2px solid #6698005c", width: '100%', paddingTop: '20px', borderRadius: '5px' }}>
+            <Col xs={24} sm={12} md={12} lg={8}>
+              <Title level={5} style={{ textAlign: 'center', fontSize: '14px' }}>Savings Comparison <span style={{ fontSize: '12px' }}> (INR/kWh)</span></Title>
+              <BarChart
+                width={Math.min(window.innerWidth * 0.9, 350)}
+                height={180} // Reduced height
+                data={dataForBarChart}
+                margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" tick={{ fontSize: 12 }} />
+                <YAxis tick={{ fontSize: 12 }} />
+                <RechartsTooltip />
+                <Legend wrapperStyle={{ fontSize: 12 }} />
+                <Bar dataKey="Savings" fill="#669800" />
+              </BarChart>
+            </Col>
+            <Col xs={24} sm={12} md={12} lg={8}>
+              <Title level={5} style={{ textAlign: 'center', fontSize: '14px' }}>Tariff Comparison <span style={{ fontSize: '12px' }}> (INR/kWh)</span></Title>
+              <LineChart
+                width={Math.min(window.innerWidth * 0.9, 350)}
+                height={180}
+                data={dataForTariffComparison}
+                margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" tick={{ fontSize: 12 }} />
+                <YAxis tick={{ fontSize: 12 }} />
+                <RechartsTooltip />
+                <Legend wrapperStyle={{ fontSize: 12 }} />
+                <Line type="monotone" dataKey="Tarrif" stroke="#669800" />
+              </LineChart>
+            </Col>
+            <Col xs={24} sm={12} md={12} lg={8}>
+              <Title level={5} style={{ textAlign: 'center', fontSize: '14px' }}>Savings Breakdown <span style={{ fontSize: '12px' }}> (INR/kWh)</span></Title>
+              <BarChart
+                width={Math.min(window.innerWidth * 0.9, 400)}
+                height={180}
+                data={dataForSavingsBreakdown}
+                margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" tick={{ fontSize: 12 }} />
+                <YAxis tick={{ fontSize: 12 }} />
+                <RechartsTooltip />
+                <Legend wrapperStyle={{ fontSize: 12 }} />
+                <Bar dataKey="Breakdown" fill="#669800" />
+              </BarChart>
+            </Col>
+          </Row>
+
+          {/* Annual Savings Report Section */}
+          <Row justify="center" align="middle" style={{ marginTop: "20px" }}>
             <Col xs={24} sm={22} md={20} lg={18} xl={16}>
-              <div style={{ backgroundColor: "#F5F6FB", padding: "20px", border: "2px solid #6698005c", borderRadius: "5px", width: '100%' }}>
+              <div style={{ backgroundColor: "white", padding: "20px", border: "2px solid #6698005c", borderRadius: "5px", width: '100%' }}>
                 {error ? (
                   <div style={{ color: "red", fontSize: "16px" }}>Error: {error}</div>
                 ) : (
@@ -109,15 +165,15 @@ const AnnualSvg = () => {
                       <Col xs={24} sm={12}>
                         <Text style={{ fontSize: '20px' }}>
                           {annualSavingResponse ? annualSavingResponse.annual_savings.toLocaleString('en-IN') : "0"}
-                          <span style={{ fontSize: '16px' }}> INR</span>
+                          <span style={{ fontSize: '14px' }}> (INR/kWh)</span>
                         </Text>
                       </Col>
                       <Col xs={24} sm={12}>
-                        <Text strong style={{ fontSize: '16px' }}>Average Savings (For your Industry Category) <span style={{ fontSize: '14px' }}>(INR/MW)</span></Text>
+                        <Text strong style={{ fontSize: '16px' }}>Average Savings (For your Industry Category) </Text>
                       </Col>
                       <Col xs={24} sm={12}>
                         <Text style={{ fontSize: '20px' }}>
-                          {annualSavingResponse ? annualSavingResponse.average_savings.toLocaleString('en-IN') : "0"}
+                          {annualSavingResponse ? annualSavingResponse.average_savings.toLocaleString('en-IN') : "0"}<span style={{ fontSize: '14px' }}> (INR/kWh)</span>
                         </Text>
                       </Col>
                       <Col xs={24} sm={12}>
@@ -129,100 +185,61 @@ const AnnualSvg = () => {
                         </Text>
                       </Col>
                     </Row>
-                    <Row justify="center" style={{ marginTop: "20px" }}>
-                      <Col>
+                    <Row justify="center" style={{ marginTop: "20px" }} gutter={[16, 16]}>
+                      <Col style={{ marginRight: "10px" }}>
                         <Button
                           type="primary"
                           onClick={() => generatePDF(createPdfContent(annualSavingResponse), requirementId)}
                           icon={<FileTextOutlined />}
-                          style={{ fontSize: '16px', width: '180px', padding: '5px' }}
+                          style={{ fontSize: "16px", width: "180px", padding: "5px" }}
                         >
                           Download Report
                         </Button>
                       </Col>
+                      <Col>
+                        {status === "active" ? (
+                          <Tooltip title="Please click to proceed">
+                            <Button
+                              type="primary"
+                              style={{
+                                backgroundColor: "#669800",
+                                borderColor: "#669800",
+                                fontSize: "20px",
+                                padding: "0 40px",
+                              }}
+                              onClick={handleContinue}
+                            >
+                              Continue {`>>`}
+                            </Button>
+                          </Tooltip>
+                        ) : (
+                          <Tooltip title="To further explore, subscribe to our plan">
+                            <Button
+                              type="primary"
+                              style={{
+                                backgroundColor: "#669800",
+                                borderColor: "#669800",
+                                fontSize: "20px",
+                                padding: "0 40px",
+                              }}
+                              onClick={handleContinue}
+                            >
+                              Continue
+                            </Button>
+                          </Tooltip>
+                        )}
+                      </Col>
                     </Row>
+
                   </div>
                 )}
               </div>
             </Col>
           </Row>
 
-          {/* Infographics Section */}
-          <Row gutter={[16, 16]} justify="center" style={{ marginTop: "20px" }}>
-            <Col xs={24} sm={12} md={12} lg={10}>
-              <Title level={4} style={{ textAlign: 'center' }}>Savings Comparison</Title>
-              <BarChart
-                width={Math.min(window.innerWidth * 0.8, 500)}
-                height={300}
-                data={dataForBarChart}
-                margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-              >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="name" />
-                <YAxis />
-                <RechartsTooltip />
-                <Legend />
-                <Bar dataKey="value" fill="#8884d8" />
-              </BarChart>
-            </Col>
-            <Col xs={24} sm={12} md={12} lg={10}>
-              <Title level={4} style={{ textAlign: 'center' }}>RE Replacement</Title>
-              <PieChart width={Math.min(window.innerWidth * 0.8, 400)} height={300}>
-                <Pie
-                  data={dataForPieChart}
-                  cx="50%"
-                  cy="50%"
-                  labelLine={false}
-                  label={({ name, percent }) => `${name}: ${(percent * 100).toFixed(2)}%`}
-                  outerRadius={80}
-                  fill="#8884d8"
-                  dataKey="value"
-                >
-                  {dataForPieChart.map((entry, index) => (
-                    <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-                  ))}
-                </Pie>
-                <RechartsTooltip />
-                <Legend />
-              </PieChart>
-            </Col>
-          </Row>
-
           {/* Continue Button */}
           <Row justify="center" style={{ marginTop: "20px" }}>
-            <Col>
-              {status === 'active' ? (
-                <Tooltip title="Please click to proceed">
-                  <Button
-                    type="primary"
-                    style={{
-                      backgroundColor: "#669800",
-                      borderColor: "#669800",
-                      fontSize: "20px",
-                      padding: "0 40px",
-                    }}
-                    onClick={handleContinue}
-                  >
-                    Continue {`>>`}
-                  </Button>
-                </Tooltip>
-              ) : (
-                <Tooltip title="To further explore, subscribe to our plan">
-                  <Button
-                    type="primary"
-                    style={{
-                      backgroundColor: "#669800",
-                      borderColor: "#669800",
-                      fontSize: "20px",
-                      padding: "0 40px",
-                    }}
-                    onClick={handleContinue}
-                  >
-                    Continue
-                  </Button>
-                </Tooltip>
-              )}
-            </Col>
+
           </Row>
         </main>
       </Spin>
