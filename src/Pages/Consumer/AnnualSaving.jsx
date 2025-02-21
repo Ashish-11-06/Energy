a9fb202f1b1e92f16d2cff4ea775303e51de8258
Ashish Bhosale <bhosaleashish311@gmail.com>
2025-02-21 10:10:16 +0530

    drawer

--- src/Pages/Consumer/AnnualSaving.jsx
+++ src/Pages/Consumer/AnnualSaving.jsx
@@ -81,154 +81,149 @@ const AnnualSvg = () => {
   return (
     <div>
       <Spin spinning={loading} tip="Loading...">
-        <main style={{ display: "flex", justifyContent: "center", alignItems: "center", padding: "20px", flexDirection: "column", height: "90vh", position: "relative", boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)" }}>
-          <div style={{ backgroundColor: "#F5F6FB", padding: "20px", border: "2px solid #6698005c", borderRadius: "5px", position: "relative", width: '80%' }}>
-            {error ? (
-              <div style={{ color: "red", fontSize: "16px" }}>Error: {error}</div>
-            ) : (
-              <div>
-                <Title level={2} className="text-center" style={{ fontSize: '24px', marginBottom: '20px' }}>
-                  Annual Savings Report
-                </Title>
-                <Row gutter={[16, 16]}>
-                  <Col span={12} style={{zIndex: 1000}}>
-                    <p strong style={{ fontSize: '16px',color:'#9a8406',cursor:'pointer' }} onMouseEnter={showRequirementModal}>Contracted Demand (MW)</p>
-                  </Col>
-                  <Col span={12} style={{ zIndex: 1000 }}>
-                    <Text style={{ fontSize: '20px', zIndex: 1000, color:'#9a8406',cursor:'pointer' }} onMouseEnter={showRequirementModal}>{` : `}
-                      {annualSavingResponse ? annualSavingResponse.contracted_demand : "0"}
-                    </Text>
-                  </Col>
-                  <Col span={12}>
-                    <Text strong style={{ fontSize: '16px' }}>Potential Savings</Text>
-                  </Col>
-                  <Col span={12}>
-                    <Text style={{ fontSize: '20px' }}>{` :  `} {` `}
-                      {annualSavingResponse ? annualSavingResponse.annual_savings.toLocaleString('en-IN') : "0"}
-                      <span style={{ fontSize: '16px' }}>{' '}INR</span>
-                    </Text>
-                  </Col>
-                  <Col span={12}>
-                    <Text strong style={{ fontSize: '16px' }}>Average Savings (For your Industry Category) <span style={{fontSize:'14px'}}>(INR/MW)</span></Text>
-                  </Col>
-                  <Col span={12}>
-                    <Text style={{ fontSize: '20px' }}>{` : `}
-                      {annualSavingResponse ? annualSavingResponse.average_savings.toLocaleString('en-IN') : "0"} <span style={{ fontSize: '16px' }}></span>
-                    </Text>
-                  </Col>
-                  <Col span={12}>
-                    <Text strong style={{ fontSize: '16px' }}>Potential RE Replacement</Text>
-                  </Col>
-                  <Col span={12}>
-                    <Text style={{ fontSize: '20px' }}>{` : `}
-                      {annualSavingResponse ? annualSavingResponse.re_replacement : "0"}%
-                    </Text>
-                  </Col>
-                </Row>
-                <div style={{ marginTop: "20px" }}>
-                  <Space wrap className="actions" style={{ marginTop: "20px", display: 'flex', justifyContent: 'space-between' }}>
-                    <Button type="primary" onClick={() => generatePDF(createPdfContent(annualSavingResponse), requirementId)} icon={<FileTextOutlined />} style={{ fontSize: '16px',width:'180px', padding: '5px', zIndex: 100 }}>
-                      Download Report
-                    </Button>
-                  </Space>
-                </div>
+        <main style={{ padding: "20px", minHeight: "90vh", position: "relative" }}>
+          <Row justify="center" align="middle">
+            <Col xs={24} sm={22} md={20} lg={18} xl={16}>
+              <div style={{ backgroundColor: "#F5F6FB", padding: "20px", border: "2px solid #6698005c", borderRadius: "5px", width: '100%' }}>
+                {error ? (
+                  <div style={{ color: "red", fontSize: "16px" }}>Error: {error}</div>
+                ) : (
+                  <div>
+                    <Title level={2} style={{ fontSize: '24px', marginBottom: '20px', textAlign: 'center' }}>
+                      Annual Savings Report
+                    </Title>
+                    <Row gutter={[16, 16]}>
+                      <Col xs={24} sm={12}>
+                        <Text strong style={{ fontSize: '16px', color: '#9a8406', cursor: 'pointer' }} onMouseEnter={showRequirementModal}>
+                          Contracted Demand (MW)
+                        </Text>
+                      </Col>
+                      <Col xs={24} sm={12}>
+                        <Text style={{ fontSize: '20px', color: '#9a8406', cursor: 'pointer' }} onMouseEnter={showRequirementModal}>
+                          {annualSavingResponse ? annualSavingResponse.contracted_demand : "0"}
+                        </Text>
+                      </Col>
+                      <Col xs={24} sm={12}>
+                        <Text strong style={{ fontSize: '16px' }}>Potential Savings</Text>
+                      </Col>
+                      <Col xs={24} sm={12}>
+                        <Text style={{ fontSize: '20px' }}>
+                          {annualSavingResponse ? annualSavingResponse.annual_savings.toLocaleString('en-IN') : "0"}
+                          <span style={{ fontSize: '16px' }}> INR</span>
+                        </Text>
+                      </Col>
+                      <Col xs={24} sm={12}>
+                        <Text strong style={{ fontSize: '16px' }}>Average Savings (For your Industry Category) <span style={{ fontSize: '14px' }}>(INR/MW)</span></Text>
+                      </Col>
+                      <Col xs={24} sm={12}>
+                        <Text style={{ fontSize: '20px' }}>
+                          {annualSavingResponse ? annualSavingResponse.average_savings.toLocaleString('en-IN') : "0"}
+                        </Text>
+                      </Col>
+                      <Col xs={24} sm={12}>
+                        <Text strong style={{ fontSize: '16px' }}>Potential RE Replacement</Text>
+                      </Col>
+                      <Col xs={24} sm={12}>
+                        <Text style={{ fontSize: '20px' }}>
+                          {annualSavingResponse ? annualSavingResponse.re_replacement : "0"}%
+                        </Text>
+                      </Col>
+                    </Row>
+                    <Row justify="center" style={{ marginTop: "20px" }}>
+                      <Col>
+                        <Button
+                          type="primary"
+                          onClick={() => generatePDF(createPdfContent(annualSavingResponse), requirementId)}
+                          icon={<FileTextOutlined />}
+                          style={{ fontSize: '16px', width: '180px', padding: '5px' }}
+                        >
+                          Download Report
+                        </Button>
+                      </Col>
+                    </Row>
+                  </div>
+                )}
               </div>
-            )}
-          </div>
-          <div>
-            {/* Infographics Section */}
-            <Row gutter={[16, 16]} style={{ marginTop: "20px" }}>
-              <Col span={12}>
-                <Title level={4} style={{ textAlign: 'center' }}>Savings Comparison</Title>
-                <BarChart
-                  width={500}
-                  height={300}
-                  data={dataForBarChart}
-                  margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+            </Col>
+          </Row>
+
+          {/* Infographics Section */}
+          <Row gutter={[16, 16]} justify="center" style={{ marginTop: "20px" }}>
+            <Col xs={24} sm={12} md={12} lg={10}>
+              <Title level={4} style={{ textAlign: 'center' }}>Savings Comparison</Title>
+              <BarChart
+                width={Math.min(window.innerWidth * 0.8, 500)}
+                height={300}
+                data={dataForBarChart}
+                margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <RechartsTooltip />
+                <Legend />
+                <Bar dataKey="value" fill="#8884d8" />
+              </BarChart>
+            </Col>
+            <Col xs={24} sm={12} md={12} lg={10}>
+              <Title level={4} style={{ textAlign: 'center' }}>RE Replacement</Title>
+              <PieChart width={Math.min(window.innerWidth * 0.8, 400)} height={300}>
+                <Pie
+                  data={dataForPieChart}
+                  cx="50%"
+                  cy="50%"
+                  labelLine={false}
+                  label={({ name, percent }) => `${name}: ${(percent * 100).toFixed(2)}%`}
+                  outerRadius={80}
+                  fill="#8884d8"
+                  dataKey="value"
                 >
-                  <CartesianGrid strokeDasharray="3 3" />
-                  <XAxis dataKey="name" />
-                  <YAxis />
-                  <RechartsTooltip />
-                  <Legend />
-                  <Bar dataKey="value" fill="#8884d8" />
-                </BarChart>
-              </Col>
-              <Col span={12}>
-                <Title level={4} style={{ textAlign: 'center' }}>RE Replacement</Title>
-                <PieChart width={400} height={300}>
-                  <Pie
-                    data={dataForPieChart}
-                    cx="50%"
-                    cy="50%"
-                    labelLine={false}
-                    label={({ name, percent }) => `${name}: ${(percent * 100).toFixed(2)}%`}
-                    outerRadius={80}
-                    fill="#8884d8"
-                    dataKey="value"
+                  {dataForPieChart.map((entry, index) => (
+                    <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                  ))}
+                </Pie>
+                <RechartsTooltip />
+                <Legend />
+              </PieChart>
+            </Col>
+          </Row>
+
+          {/* Continue Button */}
+          <Row justify="center" style={{ marginTop: "20px" }}>
+            <Col>
+              {status === 'active' ? (
+                <Tooltip title="Please click to proceed">
+                  <Button
+                    type="primary"
+                    style={{
+                      backgroundColor: "#669800",
+                      borderColor: "#669800",
+                      fontSize: "20px",
+                      padding: "0 40px",
+                    }}
+                    onClick={handleContinue}
                   >
-                    {dataForPieChart.map((entry, index) => (
-                      <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-                    ))}
-                  </Pie>
-                  <RechartsTooltip />
-                  <Legend />
-                </PieChart>
-              </Col>
-            </Row>
-          </div>
-          <div>
-            {/* Continue Button */}
-            {status === 'active' ? (
-              <Tooltip title="Please click to proceed">
-                <Button
-                  type="primary"
-                  style={{
-                    marginTop: "20px",
-                    backgroundColor: "#669800",
-                    borderColor: "#669800",
-                    fontSize: "20px",
-                    padding: "0 40px",
-                    zIndex: 1,
-                    position: 'relative',
-                  }}
-                  onClick={handleContinue}
-                >
-                  Continue {`>>`}
-                </Button>
-              </Tooltip>
-            ) : (
-              <Tooltip title="To further explore, subscribe to our plan">
-                <Button
-                  type="primary"
-                  style={{
-                    marginTop: "20px",
-                    backgroundColor: "#669800",
-                    borderColor: "#669800",
-                    fontSize: "20px",
-                    padding: "0 40px",
-                    zIndex: 1,
-                    position: 'relative',
-                  }}
-                  onClick={handleContinue}
-                >
-                  Continue
-                </Button>
-              </Tooltip>
-            )}
-          </div>
-          {/* Background Structure */}
-          <div style={{
-            position: "absolute",
-            top: "0",
-            left: "0",
-            right: "0",
-            bottom: "0",
-            background: "url('path-to-your-structure-image') no-repeat center center fixed",
-            backgroundSize: "cover",
-            opacity: 0.1,
-            zIndex: 0
-          }} />
+                    Continue {`>>`}
+                  </Button>
+                </Tooltip>
+              ) : (
+                <Tooltip title="To further explore, subscribe to our plan">
+                  <Button
+                    type="primary"
+                    style={{
+                      backgroundColor: "#669800",
+                      borderColor: "#669800",
+                      fontSize: "20px",
+                      padding: "0 40px",
+                    }}
+                    onClick={handleContinue}
+                  >
+                    Continue
+                  </Button>
+                </Tooltip>
+              )}
+            </Col>
+          </Row>
         </main>
       </Spin>
       <DemandModal
