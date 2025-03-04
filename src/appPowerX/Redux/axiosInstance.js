99f6489ef15c9bcc98533dfdf0b58c45d959818b
Aarupa <aartilahane013@gmail.com>
2025-03-04 15:16:26 +0530

    added new url

--- src/appPowerX/Redux/axiosInstance.js
+++ src/appPowerX/Redux/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    // baseURL: 'http://127.0.0.1:8000/api/powerx', // Ensure this base URL is correct
-    baseURL: 'http://localhost:5000/',
+    baseURL: 'http://127.0.0.1:8000/api/powerx', // Ensure this base URL is correct
+    // baseURL: 'http://localhost:5000/',
     headers: {
         'Content-Type': 'application/json',
     },
