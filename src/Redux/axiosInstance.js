75cc22e2b24c6f758c552d4f89972c9dcd94430f
manikerisamruddhi <manikerisamruddhi@gmail.com>
2025-03-21 09:36:18 +0530

    change url

--- src/Redux/axiosInstance.js
+++ src/Redux/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://15.207.188.206/api', // Ensure this base URL is correct
+    baseURL: 'http://15.207.188.206:8000/api', // Ensure this base URL is correct
     headers: {
         'Content-Type': 'application/json',
     },
