631970d6bc1172f5304f99d7cb8efd2070301e2d
Ashish Bhosale <bhosaleashish311@gmail.com>
2025-04-10 15:49:18 +0530

    Update WebSocket and API base URLs to new server address

--- src/Redux/axiosInstance.js
+++ src/Redux/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://172.31.16.56:8000/api', // Ensure this base URL is correct
+    baseURL: 'http://52.66.186.241:8000/api', // Ensure this base URL is correct
     headers: {
         'Content-Type': 'application/json',
     },
